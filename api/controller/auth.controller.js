4502943b29fd89249388d20f674a124c992cb9f9
Arpit Gutpa <arpitgupta050505@gmail.com>
2024-08-18 06:32:25 +0530

    complete sign in page functionality

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -20,7 +20,7 @@ export const signup =  async (req, res, next) => {
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
     try{
-        const validUser = await User.findOne({ email });
+        const validUser = await User.findOne({ email : email });
         if(!validUser) return next(errorHandler(404, 'User Not Found!'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if(!validPassword) return next(errorHandler(401, 'Wrong Credentials!'));
