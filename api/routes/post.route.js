be16931f7fadbf52c96f8d709192f4e126a13ad6
Arpit Gutpa <arpitgupta050505@gmail.com>
2024-10-26 21:42:09 +0530

    Initial commit

---
+++ api/routes/post.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { login, logout, register } from "../controller/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+
+router.post("/post", login)
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+export default router;
